f23f030560eca54a19d96d6c6090690a8bae12ee
Ezzz-Lui <ogn.lui@gmail.com>
2025-08-02 04:09:16 -0600

    feat: add additional technology icons and update layout in TooltipDock component

--- components/Portfolio/TooltipDock.tsx
+++ components/Portfolio/TooltipDock.tsx
@@ -7,7 +7,15 @@ import TypescriptIcon from "../icons/TypescriptIcon";
 import PythonIcon from "../icons/PythonIcon";
 import PostgresqlIcon from "../icons/PostgresqlIcon";
 import TailwindIcon from "../icons/TailwindIcon";
-
+import VueIcon from "../icons/VueIcon";
+import NestJsIcon from "../icons/NestIcon";
+import DjangoIcon from "../icons/DjangoIcon";
+import PiniaIcon from "../icons/PiniaIcon";
+import shadcnui from "../icons/ShadcnIcon";
+import RadixUI from "../icons/RadixUi";
+import SupabaseIcon from "../icons/SupabaseIcon";
+import Dart from "../icons/DartIcon";
+import Flutter from "../icons/FlutterIcon";
 const technologies = [
   {
     id: 1,
@@ -35,9 +43,9 @@ const technologies = [
   },
   {
     id: 5,
-    name: "PostgreSQL",
-    designation: "Database",
-    icon: PostgresqlIcon,
+    name: "Vue.js",
+    designation: "Frontend Framework",
+    icon: VueIcon,
   },
   {
     id: 6,
@@ -45,11 +53,54 @@ const technologies = [
     designation: "Styling Framework",
     icon: TailwindIcon,
   },
+  {
+    id: 7,
+    name: "NestJs",
+    designation: "Backend Framework",
+    icon: NestJsIcon,
+  },
+  {
+    id: 8,
+    name: "Django",
+    designation: "Backend Framework",
+    icon: DjangoIcon,
+  },
+  {
+    id: 9,
+    name: "Pinia",
+    designation: "State Management",
+    icon: PiniaIcon,
+  },
+  {
+    id: 11,
+    name: "Radix UI",
+    designation: "Headless UI Components",
+    icon: RadixUI,
+  },
+  {
+    id: 12,
+    name: "Supabase",
+    designation: "Database as a Service",
+    icon: SupabaseIcon,
+  },
+  {
+    id: 13,
+    name: "Dart",
+    designation: "Programming Language",
+    icon: Dart,
+  },
+  {
+    id: 14,
+    name: "Flutter",
+    designation: "Mobile Development",
+    icon: Flutter,
+  }
+
 ];
 
 export function AnimatedTooltipPreview() {
   return (
-    <div className="flex flex-row items-center justify-center mb-10 w-full mt-10">
+    <div className="flex flex-row items-center justify-center mb-10 w-full px-4 sm:px-6 lg:px-8">
       <AnimatedTooltip items={technologies} />
     </div>
   );
