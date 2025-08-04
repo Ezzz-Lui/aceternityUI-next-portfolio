5630131a9b8d632508d647a0fb03f76a4550bc16
Ezzz-Lui <ogn.lui@gmail.com>
2025-08-04 07:19:30 -0600

    feat: add projects data with descriptions and skills for portfolio

---
+++ components/data/projects.ts
@@ -0,0 +1,147 @@
+import Astro from "@/components/icons/AstroIcon";
+import MicrosoftAzure from "@/components/icons/AzureIcon";
+import Django from "@/components/icons/DjangoIcon";
+import Docker from "@/components/icons/DockerIcon";
+import JavaScript from "@/components/icons/JavascriptIcon";
+import Nextjs from "@/components/icons/NextIcon";
+import PostgreSQL from "@/components/icons/PostgresqlIcon";
+import Python from "@/components/icons/PythonIcon";
+import Supabase from "@/components/icons/SupabaseIcon";
+import TailwindCSS from "@/components/icons/TailwindIcon";
+import TypeScript from "@/components/icons/TypescriptIcon";
+import Vercel from "@/components/icons/VercelIcon";
+import ReactIcon from "../icons/ReactIcon";
+import Vue from "../icons/VueIcon";
+import NestJS from "../icons/NestIcon";
+import Pinia from "../icons/PiniaIcon";
+import Vite from "../icons/ViteIcon";
+import VueUse from "../icons/VueUseIcon";
+import Flutter from "../icons/FlutterIcon";
+import Dart from "../icons/DartIcon";
+import MaterialUI from "../icons/MaterialIcon";
+
+export const projects = [
+  {
+    title: "ReelZ - Vertical Video App",
+    description:
+      "The Benefits Calculator is a web application developed with Django that allows users to calculate salary benefits easily and quickly. The application is deployed on Azure and uses various technologies to provide an efficient and reliable user experience.",
+    skills: [
+      { name: "Flutter", icon: Flutter },
+      { name: "Dart", icon: Dart },
+      { name: "Material UI", icon: MaterialUI },
+    ],
+    date: "August 2025",
+    category: "Mobile Development",
+  },
+  {
+    title: "E-commerce Shop System",
+    description:
+      "The Benefits Calculator is a web application developed with Django that allows users to calculate salary benefits easily and quickly. The application is deployed on Azure and uses various technologies to provide an efficient and reliable user experience.",
+    skills: [
+      { name: "Vue.js", icon: Vue },
+      { name: "VueUse", icon: VueUse },
+      { name: "NestJs", icon: NestJS },
+      { name: "TailwindCSS", icon: TailwindCSS },
+      { name: "Typescript", icon: TypeScript },
+      { name: "Pinia", icon: Pinia },
+      { name: "Vite", icon: Vite },
+      { name: "PostgreSQL", icon: PostgreSQL },
+    ],
+    date: "July 2025",
+    category: "Frontend Development",
+  },
+  {
+    title: "FakeNews AI - Detector",
+    description:
+      "The Benefits Calculator is a web application developed with Django that allows users to calculate salary benefits easily and quickly. The application is deployed on Azure and uses various technologies to provide an efficient and reliable user experience.",
+    skills: [
+      { name: "NextJs", icon: Nextjs },
+      { name: "Python", icon: Python },
+      { name: "Azure", icon: MicrosoftAzure },
+      { name: "TailwindCSS", icon: TailwindCSS },
+      { name: "Vercel", icon: Vercel },
+      { name: "Typescript", icon: TypeScript },
+      { name: "React", icon: ReactIcon },
+      { name: "Django", icon: Django },
+    ],
+    date: "March 2024",
+    category: "Fullstack Development - Samsung Hackathon ",
+  },
+  {
+    title: "Benefits Calculator - ISR",
+    description:
+      "The Benefits Calculator is a web application developed with Django that allows users to calculate salary benefits easily and quickly. The application is deployed on Azure and uses various technologies to provide an efficient and reliable user experience.",
+    skills: [
+      { name: "Django", icon: Django },
+      { name: "Python", icon: Python },
+      { name: "Azure", icon: MicrosoftAzure },
+      { name: "TailwindCSS", icon: TailwindCSS },
+    ],
+    date: "March 2024",
+    category: "Backend Development",
+  },
+  {
+    title: "Point of Sale System - by Grupo Manzur",
+    description:
+      "The Point of Sale System is a web application developed with NextJs that allows users to manage their sales and inventory. The application is deployed on Vercel and uses various technologies to provide an efficient and reliable user experience.",
+    skills: [
+      { name: "Typescript", icon: TypeScript },
+      { name: "NextJs", icon: Nextjs },
+      { name: "TailwindCSS", icon: TailwindCSS },
+      { name: "Vercel", icon: Vercel },
+      { name: "Supabase", icon: Supabase },
+      { name: "Docker", icon: Docker },
+    ],
+    date: "March 2024",
+    category: "Fullstack Development",
+  },
+  {
+    title: "Final Project - Probability and Statistics ITCA 2024",
+    description:
+      "Final Examination Project - Development of statistical and probabilistic thinking. Collection of games representing probability and statistics applied in percentages such as: Coin Toss, 21 Blackjack and Dice Toss",
+    skills: [
+      { name: "Astro", icon: Astro },
+      { name: "Javascript", icon: JavaScript },
+      { name: "TailwindCSS", icon: TailwindCSS },
+      { name: "Vercel", icon: Vercel },
+    ],
+    date: "March 2024",
+    category: "Web Development",
+  },
+  {
+    title: "QueryWise AI",
+    description:
+      "It converts natural language into SQL queries, tracking real-time token usage and associated costs. This proof-of-concept offers transparency and control over resource consumption using AI.",
+    skills: [
+      { name: "NextJS", icon: Nextjs },
+      { name: "Typescript", icon: TypeScript },
+      { name: "Python", icon: Python },
+      { name: "Azure AI", icon: MicrosoftAzure },
+    ],
+    date: "March 2024",
+    category: "Artificial Intelligence",
+  },
+  {
+    title: "RSM - Final Data Analyst Project",
+    description:
+      "Analyze your sales data and propose improvement strategies as a Junior Data Analyst using SQL, data visualization and machine learning models.",
+    skills: [
+      { name: "PostgreSQL", icon: PostgreSQL },
+      { name: "Python", icon: Python },
+    ],
+    date: "March 2024",
+    category: "Data Analysis",
+  },
+  {
+    title: "Samsung Innovation Campus - Module Project",
+    description:
+      "Analyze data of C02 emissions in Central America. The project was developed using Python with Django data visualization libraries.",
+    skills: [
+      { name: "Python", icon: Python },
+      { name: "Django", icon: Django },
+      { name: "Docker", icon: Docker },
+    ],
+    date: "March 2024",
+    category: "Fullstack Development",
+  },
+];
